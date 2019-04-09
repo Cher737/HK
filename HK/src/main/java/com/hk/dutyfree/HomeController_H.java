077c460e4e328eb60367e091a713ac2840112f17
soldesk <soldesk@soldesk-PC>
2019-04-09 20:31:13 +0900

    04.09 영찬 업로드 2

--- HK/src/main/java/com/hk/dutyfree/HomeController_H.java
+++ HK/src/main/java/com/hk/dutyfree/HomeController_H.java
@@ -2,30 +2,43 @@ package com.hk.dutyfree;
 
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hk.dutyfree.product.SpecialDAO;
+import com.hk.dutyfree.rate.rateDAO;
 
 
 @Controller
 public class HomeController_H {
 	
+	@Autowired
+	private rateDAO rDAO;
 	
-
+	@Autowired
+	private SpecialDAO spDAO;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String Index(HttpServletRequest request) {
+	public String Index(HttpServletRequest request, HttpServletResponse response) {
+		rDAO.getRate(request);
+		spDAO.getSailProducts(request, response);
 		request.setAttribute("centerpage", "main/main.jsp");
 		return "index";
 	}
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "main/main.jsp");
 		return "index";
 	}
 	
 	@RequestMapping(value = "/Support.FAQ", method = RequestMethod.GET)
 	public String Support(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Support/Supportmain.jsp");
 		request.setAttribute("Surportpage", "SupportFAQ.jsp");
 		return "index";
@@ -33,12 +46,14 @@ public class HomeController_H {
 	
 	@RequestMapping(value = "/Support.Counseling", method = RequestMethod.GET)
 	public String Counseling(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Support/Supportmain.jsp");
 		request.setAttribute("Surportpage", "Counseling.jsp");
 		return "index";
 	}
 	@RequestMapping(value = "/Mypage.main", method = RequestMethod.GET)
 	public String Mypage(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "BuyList.jsp");
 		return "index";
@@ -46,12 +61,14 @@ public class HomeController_H {
 	
 	@RequestMapping(value = "/Mypage.Coupon", method = RequestMethod.GET)
 	public String MypageCoupon(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "Coupon.jsp");
 		return "index";
 	}
 	@RequestMapping(value = "/Mypage.MyCounseling", method = RequestMethod.GET)
 	public String MyCounseling(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "MyCounseling.jsp");
 		return "index";
@@ -59,6 +76,7 @@ public class HomeController_H {
 	
 	@RequestMapping(value = "/Mypage.Passport.main", method = RequestMethod.GET)
 	public String PassPortMain(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "Passport/Passportmain.jsp");
 		request.setAttribute("Passportpage", "Passportselect.jsp");
@@ -66,6 +84,7 @@ public class HomeController_H {
 	}
 	@RequestMapping(value = "/Mypage.Passport.update", method = RequestMethod.GET)
 	public String PassPortupdate(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "Passport/Passportmain.jsp");
 		request.setAttribute("Passportpage", "Passportupdate.jsp");
@@ -73,6 +92,7 @@ public class HomeController_H {
 	}
 	@RequestMapping(value = "/Mypage.Leave.Main", method = RequestMethod.GET)
 	public String LeaveMain(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/MypageMain.jsp");
 		request.setAttribute("Mypage", "LeaveMain.jsp");
 		return "index";
@@ -80,6 +100,7 @@ public class HomeController_H {
 	
 	@RequestMapping(value = "/Mypage.Buy", method = RequestMethod.GET)
 	public String Buy(HttpServletRequest request) {
+		rDAO.getRate(request);
 		request.setAttribute("centerpage", "Mypage/Buy.jsp");
 		return "index";
 	}
